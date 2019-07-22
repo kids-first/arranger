6cf7e5566c68cf33ebb0b475c85fc732c5b77368
Julien Bérubé <julien.berube@gmail.com>
2019-07-22 14:03:55 -0400

    Updated babel from mixed versions to 7.5.5, jest from 22.3.0 to 24.8.0.

--- modules/server/index.js
+++ modules/server/index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
-require('babel-polyfill');
 require('dotenv').config();
-require('@babel/register')({
-  presets: ['@babel/env'],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-object-rest-spread',
-  ],
-});
+const fs = require('fs');
+const babelCfg = fs.readFileSync('.babelrc', 'utf-8');
+require('@babel/register')(babelCfg);
+require('@babel/register')();
+
+// Necessary to polyfill Babel (replaces @babel/polyfill since 7.4.0)
+// see https://babeljs.io/docs/en/7.4.0/babel-polyfill
+require('core-js/stable');
+require('regenerator-runtime/runtime');
 
 require('./src').App();
