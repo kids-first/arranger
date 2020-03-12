7ee682f1df4f4d91343e365bb346d4b833cce574
evans-g-crsj <eg.crsj@gmail.com>
2020-03-12 14:19:17 -0400

    test(middleware): -avoiding using loop to run test so when can pinpoint exact failing test easily. -make sure tests passes (exactitude has to be verified)

--- modules/middleware/test/buildQuery/buildQueryWildcard.test.js
+++ modules/middleware/test/buildQuery/buildQueryWildcard.test.js
@@ -1,335 +1,360 @@
 import buildQuery from '../../src/buildQuery';
-test('buildQuery wildcard nested', () => {
-  const nestedFields = ['files', 'files.foo'];
 
-  const tests = [
-    {
-      input: {
-        nestedFields,
-        filters: { content: { field: 'case_id', value: ['006*'] }, op: 'in' },
-      },
-      output: { regexp: { case_id: '006.*' } },
+const nestedFields = ['files', 'files.foo'];
+
+
+test('buildQuery wildcard nested with regexp', () => {
+  const input = {
+    nestedFields,
+    filters: { content: { field: 'case_id', value: ['regexp:006.*'] }, op: 'in' },
+  };
+  const output = { regexp: { case_id: '006.*' } };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+test('buildQuery wildcard nested', () => {
+  const input = {
+    nestedFields,
+    filters: { content: { field: 'case_id', value: ['006'] }, op: 'in' },
+  };
+  const output = { terms: { 'boost': 0, case_id: ['006'] } };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+test('buildQuery wildcard nested with one regexp and one ordinary value', () => {
+  const input = {
+    nestedFields,
+    filters: {
+      content: { field: 'case_id', value: ['regexp:006*', 'v1'] },
+      op: 'in',
     },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: [
-            { content: { field: 'case_id', value: ['006*'] }, op: 'in' },
-          ],
-          op: 'and',
+  };
+  const output = {
+    bool: {
+      should: [
+        { terms: { case_id: ['v1'], boost: 0 } },
+        { regexp: { case_id: '006*' } },
+      ],
+    },
+  };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+test('buildQuery wildcard nested with one regexp and one ordinary value (op=and)', () => {
+  const input = {
+    nestedFields,
+    filters: {
+      content: [
+        { content: { field: 'case_id', value: ['regexp:006*', 'v1'] }, op: 'in' },
+      ],
+      op: 'and',
+    },
+  };
+  const output = {
+    bool: {
+      must: [
+        {
+          bool: {
+            should: [
+              { terms: { case_id: ['v1'], boost: 0 } },
+              { regexp: { case_id: '006*' } },
+            ],
+          },
         },
-      },
-      output: { bool: { must: [{ regexp: { case_id: '006.*' } }] } },
+      ],
     },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: { field: 'case_id', value: ['006*', 'v1'] },
+  };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+test('buildQuery wildcard nested with multiple contents with one containing a regex', () => {
+  const input = {
+    nestedFields,
+    filters: {
+      content: [
+        { content: { field: 'case_id', value: ['regexp:006*', 'v1'] }, op: 'in' },
+        {
+          content: { field: 'project.primary_site', value: ['Brain'] },
           op: 'in',
         },
-      },
-      output: {
-        bool: {
-          should: [
-            { terms: { case_id: ['v1'], boost: 0 } },
-            { regexp: { case_id: '006.*' } },
-          ],
-        },
-      },
+      ],
+      op: 'and',
     },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: [
-            { content: { field: 'case_id', value: ['006*', 'v1'] }, op: 'in' },
-          ],
-          op: 'and',
+  };
+  const output = {
+    bool: {
+      must: [
+        {
+          bool: {
+            should: [
+              { terms: { case_id: ['v1'], boost: 0 } },
+              { regexp: { case_id: '006*' } },
+            ],
+          },
         },
-      },
-      output: {
-        bool: {
-          must: [
-            {
-              bool: {
-                should: [
-                  { terms: { case_id: ['v1'], boost: 0 } },
-                  { regexp: { case_id: '006.*' } },
-                ],
-              },
-            },
-          ],
-        },
-      },
+        { terms: { 'project.primary_site': ['Brain'], boost: 0 } },
+      ],
     },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: [
-            { content: { field: 'case_id', value: ['006*', 'v1'] }, op: 'in' },
-            {
-              content: { field: 'project.primary_site', value: ['Brain'] },
-              op: 'in',
-            },
-          ],
-          op: 'and',
-        },
-      },
-      output: {
-        bool: {
-          must: [
-            {
-              bool: {
-                should: [
-                  { terms: { case_id: ['v1'], boost: 0 } },
-                  { regexp: { case_id: '006.*' } },
-                ],
-              },
-            },
-            { terms: { 'project.primary_site': ['Brain'], boost: 0 } },
-          ],
-        },
-      },
-    },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: [
-            { content: { field: 'files.foo.name', value: 'cname*' }, op: '=' },
-          ],
-          op: 'and',
-        },
-      },
-      output: {
-        bool: {
-          must: [
-            {
-              nested: {
-                path: 'files',
-                query: {
-                  bool: {
-                    must: [
-                      {
-                        nested: {
-                          path: 'files.foo',
-                          query: {
-                            bool: {
-                              must: [
-                                { regexp: { 'files.foo.name': 'cname.*' } },
-                              ],
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: [
-            {
-              content: {
-                field: 'files.foo.name',
-                value: ['*cname', 'cn*me', 'cname*'],
-              },
-              op: 'in',
-            },
-          ],
-          op: 'and',
-        },
-      },
-      output: {
-        bool: {
-          must: [
-            {
-              bool: {
-                should: [
-                  {
-                    nested: {
-                      path: 'files',
-                      query: {
-                        bool: {
-                          must: [
-                            {
-                              nested: {
-                                path: 'files.foo',
-                                query: {
-                                  bool: {
-                                    must: [
-                                      {
-                                        regexp: { 'files.foo.name': '.*cname' },
-                                      },
-                                    ],
-                                  },
-                                },
-                              },
-                            },
-                          ],
-                        },
-                      },
-                    },
-                  },
-                  {
-                    nested: {
-                      path: 'files',
-                      query: {
-                        bool: {
-                          must: [
-                            {
-                              nested: {
-                                path: 'files.foo',
-                                query: {
-                                  bool: {
-                                    must: [
-                                      {
-                                        regexp: { 'files.foo.name': 'cn.*me' },
-                                      },
-                                    ],
-                                  },
-                                },
-                              },
-                            },
-                          ],
-                        },
-                      },
-                    },
-                  },
-                  {
-                    nested: {
-                      path: 'files',
-                      query: {
-                        bool: {
-                          must: [
-                            {
-                              nested: {
-                                path: 'files.foo',
-                                query: {
-                                  bool: {
-                                    must: [
-                                      {
-                                        regexp: { 'files.foo.name': 'cname.*' },
-                                      },
-                                    ],
-                                  },
-                                },
-                              },
-                            },
-                          ],
-                        },
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      },
-    },
-    {
-      input: {
-        nestedFields,
-        filters: {
-          content: [
-            { content: { field: 'files.foo.name1', value: '*cname' }, op: '=' },
-            { content: { field: 'files.foo.name2', value: 'cn*me' }, op: '=' },
-            { content: { field: 'files.foo.name3', value: 'cname*' }, op: '=' },
-          ],
-          op: 'and',
-        },
-      },
-      output: {
-        bool: {
-          must: [
-            {
-              nested: {
-                path: 'files',
-                query: {
-                  bool: {
-                    must: [
-                      {
-                        nested: {
-                          path: 'files.foo',
-                          query: {
-                            bool: {
-                              must: [
-                                { regexp: { 'files.foo.name1': '.*cname' } },
-                              ],
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                },
-              },
-            },
-            {
-              nested: {
-                path: 'files',
-                query: {
-                  bool: {
-                    must: [
-                      {
-                        nested: {
-                          path: 'files.foo',
-                          query: {
-                            bool: {
-                              must: [
-                                { regexp: { 'files.foo.name2': 'cn.*me' } },
-                              ],
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                },
-              },
-            },
-            {
-              nested: {
-                path: 'files',
-                query: {
-                  bool: {
-                    must: [
-                      {
-                        nested: {
-                          path: 'files.foo',
-                          query: {
-                            bool: {
-                              must: [
-                                { regexp: { 'files.foo.name3': 'cname.*' } },
-                              ],
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
-  ];
+  };
 
-  tests.forEach(({ input, output }) => {
-    const actualOutput = buildQuery(input);
-    expect(actualOutput).toEqual(output);
-  });
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
 });
+
+
+test('buildQuery wildcard nested (op "=")', () => {
+  const input = {
+    nestedFields,
+    filters: {
+      content: [
+        { content: { field: 'files.foo.name', value: 'regexp:cname*' }, op: '=' },
+      ],
+      op: 'and',
+    },
+  };
+  const output = {
+    bool: {
+      must: [
+        {
+          nested: {
+            path: 'files',
+            query: {
+              bool: {
+                must: [
+                  {
+                    nested: {
+                      path: 'files.foo',
+                      query: {
+                        bool: {
+                          must: [
+                            { regexp: { 'files.foo.name': 'cname*' } },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+test('buildQuery wildcard nested multiple regex', () => {
+  const input = {
+    nestedFields,
+    filters: {
+      content: [
+        {
+          content: {
+            field: 'files.foo.name',
+            value: ['regexp:*cname', 'regexp:cn*me', 'regexp:cname*'],
+          },
+          op: 'in',
+        },
+      ],
+      op: 'and',
+    },
+  };
+  const output = {
+    bool: {
+      must: [
+        {
+          bool: {
+            should: [
+              {
+                nested: {
+                  path: 'files',
+                  query: {
+                    bool: {
+                      must: [
+                        {
+                          nested: {
+                            path: 'files.foo',
+                            query: {
+                              bool: {
+                                must: [
+                                  {
+                                    regexp: { 'files.foo.name': '*cname' },
+                                  },
+                                ],
+                              },
+                            },
+                          },
+                        },
+                      ],
+                    },
+                  },
+                },
+              },
+              {
+                nested: {
+                  path: 'files',
+                  query: {
+                    bool: {
+                      must: [
+                        {
+                          nested: {
+                            path: 'files.foo',
+                            query: {
+                              bool: {
+                                must: [
+                                  {
+                                    regexp: { 'files.foo.name': 'cn*me' },
+                                  },
+                                ],
+                              },
+                            },
+                          },
+                        },
+                      ],
+                    },
+                  },
+                },
+              },
+              {
+                nested: {
+                  path: 'files',
+                  query: {
+                    bool: {
+                      must: [
+                        {
+                          nested: {
+                            path: 'files.foo',
+                            query: {
+                              bool: {
+                                must: [
+                                  {
+                                    regexp: { 'files.foo.name': 'cname*' },
+                                  },
+                                ],
+                              },
+                            },
+                          },
+                        },
+                      ],
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+
+test('buildQuery wildcard nested multiple regex and multiple contents', () => {
+  const input = {
+    nestedFields,
+    filters: {
+      content: [
+        { content: { field: 'files.foo.name1', value: 'regexp:*cname' }, op: '=' },
+        { content: { field: 'files.foo.name2', value: 'regexp:cn*me' }, op: '=' },
+        { content: { field: 'files.foo.name3', value: 'regexp:cname*' }, op: '=' },
+      ],
+      op: 'and',
+    },
+  };
+  const output = {
+    bool: {
+      must: [
+        {
+          nested: {
+            path: 'files',
+            query: {
+              bool: {
+                must: [
+                  {
+                    nested: {
+                      path: 'files.foo',
+                      query: {
+                        bool: {
+                          must: [
+                            { regexp: { 'files.foo.name1': '*cname' } },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+        {
+          nested: {
+            path: 'files',
+            query: {
+              bool: {
+                must: [
+                  {
+                    nested: {
+                      path: 'files.foo',
+                      query: {
+                        bool: {
+                          must: [
+                            { regexp: { 'files.foo.name2': 'cn*me' } },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+        {
+          nested: {
+            path: 'files',
+            query: {
+              bool: {
+                must: [
+                  {
+                    nested: {
+                      path: 'files.foo',
+                      query: {
+                        bool: {
+                          must: [
+                            { regexp: { 'files.foo.name3': 'cname*' } },
+                          ],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  const actualOutput = buildQuery(input);
+  expect(actualOutput).toEqual(output);
+});
+
+
+
+
